2144066ac8b39b1297534cf7830bfa5c359fc927
TheCze <danielczeppel@gmail.com>
2018-04-21 14:03:24 +0200

    changed file formating

--- td-racing/core/src/com/mygdx/game/Car.java
+++ td-racing/core/src/com/mygdx/game/Car.java
@@ -4,33 +4,34 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 
 public class Car {
-Body body;
-float maxspeed=80;
-float accelarition=10;
-float armor=0;
-float brakepower=20;
-float steerpower=5;
-public Car(Body b) {
-	this.body=b;
-}
+	Body body;
+	float maxspeed = 80;
+	float accelarition = 10;
+	float armor = 0;
+	float brakepower = 20;
+	float steerpower = 5;
 
-public void accelarate() {
-	body.applyForceToCenter(new Vector2(accelarition,0), true);
-}
+	public Car(Body b) {
+		this.body = b;
+	}
 
-public void brake() {
-	body.applyForceToCenter(new Vector2(brakepower*-1,0), true);
-}
+	public void accelarate() {
+		body.applyForceToCenter(new Vector2(accelarition, 0), true);
+	}
 
-public void steerLeft() {
-	body.applyTorque(steerpower, true);
-}
+	public void brake() {
+		body.applyForceToCenter(new Vector2(brakepower * -1, 0), true);
+	}
 
-public void steerRight() {
-	body.applyTorque(steerpower*-1, true);
-}
+	public void steerLeft() {
+		body.applyTorque(steerpower, true);
+	}
 
-public void hitEnemy(Enemy e) {
-	
-}
+	public void steerRight() {
+		body.applyTorque(steerpower * -1, true);
+	}
+
+	public void hitEnemy(Enemy e) {
+
+	}
 }
