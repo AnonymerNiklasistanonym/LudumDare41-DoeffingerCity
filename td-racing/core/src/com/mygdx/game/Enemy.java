2d9c378d21ab5d8da2a8db45dd1bd13876c22037
TheCze <danielczeppel@gmail.com>
2018-04-21 22:02:09 +0200

    Made enemy generation easier

--- td-racing/core/src/com/mygdx/game/Enemy.java
+++ td-racing/core/src/com/mygdx/game/Enemy.java
@@ -3,10 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.game.gamestate.state.PlayState;
 
 public abstract class Enemy extends BodyDef{
 	public Body body;
@@ -20,8 +24,28 @@ public abstract class Enemy extends BodyDef{
 	
 	public boolean tot=false;
 	
-	
+	public Enemy(World w, Sprite sprite, Sprite deadsprite) {
+		this.speed = 80;
+		this.health = 10;
+		this.saussehen = sprite;
+		this.stot=deadsprite;
+		
+		BodyDef bodydef = new BodyDef();
+		bodydef.type = BodyDef.BodyType.DynamicBody;
+		bodydef.position.set(MathUtils.random(1280)*PlayState.PIXEL_TO_METER, MathUtils.random(720)*PlayState.PIXEL_TO_METER);
 
+		body = w.createBody(bodydef);
+		CircleShape enemyCircle = new CircleShape();
+		enemyCircle.setRadius(saussehen.getHeight()*0.35f);;
+		FixtureDef fdef = new FixtureDef();
+		fdef.shape = enemyCircle;
+		fdef.filter.categoryBits=PlayState.ENEMY_BOX;
+		fdef.filter.categoryBits=PlayState.PLAYER_BOX;
+		
+		body.createFixture(fdef);
+		body.setUserData(this);
+		this.world=w;
+	}
 	
 	public void startMove() {
 		body.applyForceToCenter(new Vector2(speed, 0), true);
