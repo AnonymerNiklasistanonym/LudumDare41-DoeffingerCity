826ea85e258f1b654166a0b1bd8647ba25a899a4
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-04-21 22:54:29 +0200

    Fixed collision listener interface the second time

--- td-racing/core/src/com/mygdx/game/CollisionListener.java
+++ td-racing/core/src/com/mygdx/game/CollisionListener.java
@@ -4,21 +4,21 @@ import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.Manifold;
-import com.mygdx.game.gamestate.state.PlayState;
 import com.mygdx.game.objects.Checkpoint;
 import com.mygdx.game.objects.Tower;
 
 public class CollisionListener implements ContactListener {
-	PlayState playstate;
+	
+	final CollisionCallbackInterface collisionCallbackInterface;
 
-	public CollisionListener(PlayState playState) {
-		this.playstate = playState;
+	public CollisionListener(CollisionCallbackInterface collisionCallbackInterface) {
+		this.collisionCallbackInterface = collisionCallbackInterface;
 	}
 
 	@Override
 	public void beginContact(Contact contact) {
-		Object a = contact.getFixtureA().getBody().getUserData();
-		Object b = contact.getFixtureB().getBody().getUserData();
+		final Object a = contact.getFixtureA().getBody().getUserData();
+		final Object b = contact.getFixtureB().getBody().getUserData();
 
 		// if one of the objects is a car
 		if (a instanceof Car || b instanceof Car) {
@@ -26,27 +26,27 @@ public class CollisionListener implements ContactListener {
 			// and the other object is an Enemy
 			if (a instanceof Enemy || b instanceof Enemy) {
 				if (a instanceof Enemy)
-					this.playstate.collisionCarEnemy((Car) b, (Enemy) a);
+					this.collisionCallbackInterface.collisionCarEnemy((Car) b, (Enemy) a);
 				else
-					this.playstate.collisionCarEnemy((Car) a, (Enemy) b);
+					this.collisionCallbackInterface.collisionCarEnemy((Car) a, (Enemy) b);
 				return;
 			}
 
 			// and the other object is a Checkpoint
 			if (a instanceof Checkpoint || b instanceof Checkpoint) {
 				if (a instanceof Checkpoint)
-					this.playstate.collisionCarCheckpoint((Car) b, (Checkpoint) a);
+					this.collisionCallbackInterface.collisionCarCheckpoint((Car) b, (Checkpoint) a);
 				else
-					this.playstate.collisionCarCheckpoint((Car) a, (Checkpoint) b);
+					this.collisionCallbackInterface.collisionCarCheckpoint((Car) a, (Checkpoint) b);
 				return;
 			}
 
 			// and the other object is a Checkpoint
 			if (a instanceof Tower || b instanceof Tower) {
 				if (a instanceof Tower)
-					this.playstate.collisionCarTower((Car) b, (Tower) a);
+					this.collisionCallbackInterface.collisionCarTower((Car) b, (Tower) a);
 				else
-					this.playstate.collisionCarTower((Car) a, (Tower) b);
+					this.collisionCallbackInterface.collisionCarTower((Car) a, (Tower) b);
 				return;
 			}
 		}
