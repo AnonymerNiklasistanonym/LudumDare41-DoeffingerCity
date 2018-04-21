6b682d510a87576af8518a241b3f5f59fe88654c
TheCze <danielczeppel@gmail.com>
2018-04-21 13:43:58 +0200

    Added Car, Enemy and World

--- td-racing/core/src/com/mygdx/game/MainGame.java
+++ td-racing/core/src/com/mygdx/game/MainGame.java
@@ -5,21 +5,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	Texture teststrecke;
+	World world;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 		teststrecke=new Texture("maps/Test.png");
+		world=new World(new Vector2(0,0),true);
+		
 	}
 
+	public void getInput() {
+		
+	}
+	
+	public void updateGame() {
+		
+	}
+	
+	
 	@Override
 	public void render () {
+		getInput();
+		updateGame();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
@@ -30,6 +44,6 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		teststrecke.dispose();
 	}
 }
