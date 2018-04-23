f59a7944de2fb23123d3823fa72924b1221027dc
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-04-23 21:40:51 +0200

    Faster method for balancing

--- td-racing/core/src/com/mygdx/game/EnemyWaveEntry.java
+++ td-racing/core/src/com/mygdx/game/EnemyWaveEntry.java
@@ -57,6 +57,25 @@ public class EnemyWaveEntry {
 		return entries;
 	}
 	
+	public static EnemyWaveEntry[] createEnemyEntries(
+			final Vector2 entryPoint, final int entryTime, final int smallNumber,
+			final int fatNumber,
+			final int bicycleNumber) {
+		
+		final EnemyWaveEntry[] entries = new EnemyWaveEntry[smallNumber + fatNumber + bicycleNumber];
+		
+		final float countingTime = 0.2f;
+		int counter = 0;
+		
+		for (int i = 0; i < smallNumber; i++)
+			entries[i] = new EnemyWaveEntry(entryTime + counter++ * countingTime, entryPoint, ENEMY_SMALL);
+		for (int i = 0 + smallNumber; i < fatNumber + smallNumber; i++)
+			entries[i] = new EnemyWaveEntry(entryTime + counter++ * countingTime, entryPoint, ENEMY_FAT);
+		for (int i = 0 + smallNumber + fatNumber; i < bicycleNumber + smallNumber + fatNumber; i++)
+			entries[i] = new EnemyWaveEntry(entryTime + counter++ * countingTime, entryPoint, ENEMY_BYCICLE);
+		return entries;
+	}
+	
 	public static Enemy createEnemy(final EnemyWaveEntry entry, final World world, final MainMap map) {
 		switch (entry.getId()) {
 		case ENEMY_SMALL:
