2ee4a45376f0e8898d42231da9d3661c656a211e
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-04-24 02:39:54 +0200

    Fixed name

--- td-racing/core/src/com/mygdx/game/MainGame.java
+++ td-racing/core/src/com/mygdx/game/MainGame.java
@@ -36,7 +36,7 @@ public class MainGame extends ApplicationAdapter {
 	/**
 	 * Name of the game
 	 */
-	public final static String GAME_NAME = "LudumDare 41 - TnT (Track n` Towers)";
+	public final static String GAME_NAME = "LudumDare 41 - TnT (Track `n Towers)";
 	/**
 	 * Width of the game screen (the window)
 	 */
