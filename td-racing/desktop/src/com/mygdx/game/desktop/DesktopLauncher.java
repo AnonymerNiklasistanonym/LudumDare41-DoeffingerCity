0c6c0a068a9d03b70fedda8ef0be669a29da929f
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2018-04-24 03:46:05 +0200

    Updated game icon

--- td-racing/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ td-racing/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -15,7 +15,7 @@ public class DesktopLauncher {
 		config.title = MainGame.GAME_NAME;
 		
 		// add application icon that will be shown if the program is running
-		for (int size : new int[] { 16, 32, 128 }) {
+		for (int size : new int[] { 16, 32, 64 }) {
 			config.addIcon("icon/icon_" + size + ".png", FileType.Internal);
 		}
 		new LwjglApplication(new MainGame(), config);
